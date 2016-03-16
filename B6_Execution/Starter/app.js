17036f3b39c78de22465cc8d59710d02f61c454c
Adam <miknevinas@gmail.com>
2016-03-16 11:28:42 -0600

    variable environments

--- B6_Execution/Starter/app.js
+++ B6_Execution/Starter/app.js
@@ -9,3 +9,21 @@ console.log(a); //undefined
 var a = 'Hello World!';
 
 console.log(a); //hello, world
+
+/////////
+
+function a() {
+    console.log("a");
+    b(); //runs second 
+    var d = "d"; //runs fourth
+    console.log(d);
+}
+
+function b() {
+    console.log("b");
+    var c = "c"; //runs third
+    console.log(c);
+}
+
+a(); //runs first
+var d; //runs last
---
+++ B9_VariableEnvironments/Starter/app.js
@@ -0,0 +1,16 @@
+function b() {
+    var myVar = 3; //fifth - new myVar is declared within scope of function b
+    console.log(myVar);
+    return myVar;
+}
+
+function a() {
+    var myVar = 2; //third - new myVar is declared within scope of function a
+    console.log(myVar);
+    b(); //fourth - b is invoked
+}
+
+var myVar = 1; //first - myVar is declared
+console.log(myVar);
+a(); //second - a is invoked
+console.log(myVar);
