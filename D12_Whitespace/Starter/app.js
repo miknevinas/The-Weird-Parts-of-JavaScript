325681d6ef29564da59f3d5855a6497fed93317b
Adam <miknevinas@gmail.com>
2016-03-28 13:07:40 -0600

    whitespace

---
+++ D12_Whitespace/Starter/app.js
@@ -0,0 +1,22 @@
+var 
+    //comments do not interrupt the code
+    firstname, 
+    
+    
+    lastname, 
+    
+    
+    
+    
+    
+    
+    
+    language;
+
+var person = {
+    //works inside an object literal
+    firstname: "Adam",
+    lastname: "McNevin"
+}
+
+console.log(person);
